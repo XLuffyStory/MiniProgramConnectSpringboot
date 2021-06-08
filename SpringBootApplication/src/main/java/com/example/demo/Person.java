b87acf796cde99b2070793a54f07cb8b1cda926e
XLuffyStory <XLuffyStory@users.noreply.github.com>
2021-06-08 22:16:08 +0800

    Simple Demo

---
+++ SpringBootApplication/src/main/java/com/example/demo/Person.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+public class Person {
+
+	private String name;
+	private String pwd;
+
+	public Person() {
+	}
+
+	public Person(String name, String pwd) {
+		this.name = name;
+		this.pwd = pwd;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+}
---
+++ SpringBootApplication/src/main/java/com/example/demo/PersonController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+	@GetMapping("/person")
+	public Person getUserInfo() {
+		Person person = new Person("guest", "123456");
+		return person;
+	}
+}
